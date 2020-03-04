b6a3ed91359d4bdf3ad0c147ca61fca8e4e2f423
SODEC <contact@resabilletcse.com>
2020-03-04 14:21:43 +0100

    [CORE] - Refactoring Install Système

--- database/seeds/TutorielCommentSeeder.php
+++ database/seeds/TutorielCommentSeeder.php
@@ -13,22 +13,6 @@ class TutorielCommentSeeder extends Seeder
      */
     public function run(Generator $faker)
     {
-        $tutos = \App\Model\Tutoriel\Tutoriel::get();
-        $user = \App\User::count();
 
-        foreach ($tutos as $tuto) {
-            for ($i = 0; $i < rand(0, 10); $i++) {
-                $published = rand(0,1);
-                if($published == 1){$published_at = now()->subDays(rand(0,720));}else{$published_at = null;}
-
-                \App\Model\Tutoriel\TutorielComment::create([
-                    "tutoriel_id"   => $tuto->id,
-                    "user_id"   => rand(1, $user),
-                    "content"   => $faker->realText(rand(50, 2000)),
-                    "published" => $published,
-                    "published_at" => $published_at
-                ]);
-            }
-        }
     }
 }
