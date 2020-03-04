8d1588e0d98cc075bbef59bdb87b973597eae30a
SODEC <contact@resabilletcse.com>
2020-03-04 15:57:55 +0100

    [CORE] - Refactoriser la fonction de génération de donnée

--- app/Http/Controllers/Api/Admin/AdminController.php
+++ app/Http/Controllers/Api/Admin/AdminController.php
@@ -66,194 +66,156 @@ class AdminController extends BaseController
         $this->tutorielCommentRepository = $tutorielCommentRepository;
     }
 
-    public function loadSignalement()
+    public function loadSignalement(Request $request)
     {
-        ob_start();
-        ?>
-        <?= $this->signAsset(); ?>
-        <?= $this->signBlog(); ?>
-        <?= $this->signBlogComment(); ?>
-        <?= $this->signTutoriel(); ?>
-        <?= $this->signTutorielComment(); ?>
-        <?php
-        $content = ob_get_clean();
+        $arrs = collect();
+        $assets = $this->assetRepository->allWithLimit(null);
+        foreach ($assets as $asset) {
+            if ($asset->downloadLink == null) {
+                $arrs->push([
+                    "state" => 'warning',
+                    "sector" => "Objets",
+                    "titre" => $asset->designation,
+                    "message" => "Cette objet n'à pas de lien de téléchargement",
+                    "action" => [
+                        "btn" => "view",
+                        "route" => ""
+                    ]
+                ]);
+            }
+            if ($asset->published == 0) {
+                $arrs->push([
+                    "state" => 'warning',
+                    "sector" => "Objets",
+                    "titre" => $asset->designation,
+                    "message" => "Cette objet n'est pas publier",
+                    "action" => [
+                        "btn" => "view",
+                        "route" => ""
+                    ], [
+                        "btn" => "publish",
+                        "route" => ""
+                    ]
+                ]);
+            }
+            if($asset->pricing == 1 && $asset->price == null) {
+                $arrs->push([
+                    "state" => 'danger',
+                    "sector" => "Objets",
+                    "titre" => $asset->designation,
+                    "message" => "Cette objet à activer le prix mais le prix est innexistant !",
+                    "action" => [
+                        "btn" => "view",
+                        "route" => ""
+                    ]
+                ]);
+            }
+        }
 
-        return $this->sendResponse($content, "Contenue");
-    }
+        $blogs = $this->blogRepository->list();
+        foreach ($blogs as $blog) {
+            if($blog->twitter == 1 && $blog->twitterText == null) {
+                $arrs->push([
+                    "state" => 'warning',
+                    "sector" => "Blog",
+                    "titre" => $blog->title,
+                    "message" => "Cette article doit être poster sur twitter mais n'à aucun texte de définie",
+                    "action" => [
+                        "btn" => "view",
+                        "route" => ""
+                    ]
+                ]);
+            }
+            if(count($blog->tags) == 0) {
+                $arrs->push([
+                    "state" => 'info',
+                    "sector" => "Blog",
+                    "titre" => $blog->title,
+                    "message" => "Cette article n'à aucun tags de définie",
+                    "action" => [
+                        "btn" => "view",
+                        "route" => ""
+                    ]
+                ]);
+            }
+        }
 
-    private function signAsset()
-    {
-        $datas = $this->assetRepository->allWithLimit(null);
-        ob_start();
-        ?>
-        <?php foreach ($datas as $data): ?>
-            <?php if($data->downloadLink == null): ?>
-            <tr class="table-warning">
-                <td>Objet</td>
-                <td><?= $data->designation; ?></td>
-                <td>Cette objet n'à pas de lien de téléchargement</td>
-                <td class="text-right">
-                    <a href=""><i class="la la-eye"></i> </a>
-                </td>
-            </tr>
-            <?php endif; ?>
-        <?php if($data->published == 0): ?>
-            <tr class="table-warning">
-                <td>Objet</td>
-                <td><?= $data->designation; ?></td>
-                <td>Cette objet n'est pas publier</td>
-                <td class="text-right">
-                    <a href=""><i class="la la-eye"></i> </a>
-                </td>
-            </tr>
-        <?php endif; ?>
-        <?php if($data->pricing == 1 && $data->price == null): ?>
-            <tr class="table-danger">
-                <td>Objet</td>
-                <td><?= $data->designation; ?></td>
-                <td>Cette objet à activer le prix mais le prix est innexistant !</td>
-                <td class="text-right">
-                    <a href=""><i class="la la-eye"></i> </a>
-                </td>
-            </tr>
-        <?php endif; ?>
-        <?php endforeach; ?>
-        <?php
-        $content = ob_get_clean();
+        $blogComments = $this->blogCommentRepository->all();
+        foreach ($blogComments as $comment) {
+            if($comment->state == 0) {
+                $arrs->push([
+                    "state" => 'danger',
+                    "sector" => "Blog | Commentaire",
+                    "titre" => $comment->blog->title."<br><i>Commentaire N°".$comment->id."</i>",
+                    "message" => "Ce commentaire n'est pas poster",
+                    "action" => [
+                        "btn" => "view",
+                        "route" => ""
+                    ],
+                    [
+                        "btn" => "publish",
+                        "route" => ""
+                    ]
+                ]);
+            }
+        }
 
-        return $content;
-    }
+        $tutoriels = $this->tutorielRepository->all();
+        foreach ($tutoriels as $tutoriel) {
+            if($tutoriel->published == 0) {
+                $arrs->push([
+                    "state" => 'warning',
+                    "sector" => "Tutoriel",
+                    "titre" => $tutoriel->title,
+                    "message" => "Ce tutoriel n'est pas publier",
+                    "action" => [
+                        "btn" => "view",
+                        "route" => ""
+                    ],
+                    [
+                        "btn" => "publish",
+                        "route" => ""
+                    ]
+                ]);
+            }
+            if($tutoriel->pathVideo == null) {
+                $arrs->push([
+                    "state" => 'warning',
+                    "sector" => "Tutoriel",
+                    "titre" => $tutoriel->title,
+                    "message" => "Ce tutoriel n'à pas de lien vidéo",
+                    "action" => [
+                        "btn" => "view",
+                        "route" => ""
+                    ]
+                ]);
+            }
+            if($tutoriel->source == 1 && Storage::disk('sftp')->exists('video/tutoriel'.$tutoriel->id.'/source.zip') == false) {
+                $arrs->push([
+                    "state" => 'danger',
+                    "sector" => "Tutoriel",
+                    "titre" => $tutoriel->title,
+                    "message" => "Ce tutoriel n'à pas de source disponible",
+                    "action" => [
+                        "btn" => "view",
+                        "route" => ""
+                    ]
+                ]);
+            }
+            if($tutoriel->demo == 1 && $tutoriel->linkDemo == null) {
+                $arrs->push([
+                    "state" => 'danger',
+                    "sector" => "Tutoriel",
+                    "titre" => $tutoriel->title,
+                    "message" => "Ce tutoriel n'à pas de lien de démo disponible",
+                    "action" => [
+                        "btn" => "view",
+                        "route" => ""
+                    ]
+                ]);
+            }
+        }
 
-    private function signBlog() {
-        $datas = $this->blogRepository->list();
-        ob_start();
-        ?>
-        <?php foreach ($datas as $data): ?>
-            <?php if($data->twitter == 1 && $data->twitterText == null): ?>
-                <tr class="table-warning">
-                    <td>Blog</td>
-                    <td><?= $data->title; ?></td>
-                    <td>Cette article doit être poster sur twitter mais n'à aucun texte de définie</td>
-                    <td class="text-right">
-                        <a href=""><i class="la la-eye"></i> </a>
-                    </td>
-                </tr>
-            <?php endif; ?>
-            <?php if(count($data->tags) == 0): ?>
-                <tr class="table-info">
-                    <td>Blog</td>
-                    <td><?= $data->title; ?></td>
-                    <td>Cette article n'à aucun tags de définie</td>
-                    <td class="text-right">
-                        <a href=""><i class="la la-eye"></i> </a>
-                    </td>
-                </tr>
-            <?php endif; ?>
-        <?php endforeach; ?>
-        <?php
-        $content = ob_get_clean();
-
-        return $content;
-    }
-
-    private function signBlogComment() {
-        $datas = $this->blogCommentRepository->all();
-        ob_start();
-        ?>
-        <?php foreach ($datas as $data): ?>
-            <?php if($data->state == 0): ?>
-                <tr class="table-danger">
-                    <td>Commentaire / Blog</td>
-                    <td><?= $data->blog->title; ?><br>
-                        <i>Commentaire: <?= $data->id; ?></i>
-                    </td>
-                    <td>Ce commentaire n'est pas poster</td>
-                    <td class="text-right">
-                        <a href=""><i class="la la-eye"></i> </a>
-                    </td>
-                </tr>
-            <?php endif; ?>
-        <?php endforeach; ?>
-        <?php
-        $content = ob_get_clean();
-
-        return $content;
-    }
-
-    private function signTutoriel()
-    {
-        $datas = $this->tutorielRepository->all();
-        ob_start();
-        ?>
-        <?php foreach ($datas as $data): ?>
-        <?php if($data->published == 0): ?>
-            <tr class="table-warning">
-                <td>Tutoriel</td>
-                <td><?= $data->title; ?></td>
-                <td>Ce tutoriel n'est pas publier</td>
-                <td class="text-right">
-                    <a href=""><i class="la la-eye"></i> </a>
-                </td>
-            </tr>
-        <?php endif; ?>
-        <?php if($data->pathVideo == null): ?>
-            <tr class="table-warning">
-                <td>Tutoriel</td>
-                <td><?= $data->title; ?></td>
-                <td>Ce tutoriel n'à pas de lien vidéo</td>
-                <td class="text-right">
-                    <a href=""><i class="la la-eye"></i> </a>
-                </td>
-            </tr>
-        <?php endif; ?>
-        <?php if($data->source == 1 && Storage::disk('sftp')->exists('video/tutoriel/'.$data->id.'/source.zip') == false): ?>
-            <tr class="table-danger">
-                <td>Tutoriel</td>
-                <td><?= $data->title; ?></td>
-                <td>Ce tutoriel n'à pas de source disponible</td>
-                <td class="text-right">
-                    <a href=""><i class="la la-eye"></i> </a>
-                </td>
-            </tr>
-        <?php endif; ?>
-        <?php if($data->demo == 1 && $data->linkDemo == null): ?>
-            <tr class="table-danger">
-                <td>Tutoriel</td>
-                <td><?= $data->title; ?></td>
-                <td>Ce tutoriel n'à pas de lien de démo disponible</td>
-                <td class="text-right">
-                    <a href=""><i class="la la-eye"></i> </a>
-                </td>
-            </tr>
-        <?php endif; ?>
-    <?php endforeach; ?>
-        <?php
-        $content = ob_get_clean();
-
-        return $content;
-    }
-
-    private function signTutorielComment() {
-        $datas = $this->tutorielCommentRepository->all();
-        ob_start();
-        ?>
-        <?php foreach ($datas as $data): ?>
-            <?php if($data->published == 0): ?>
-                <tr class="table-danger">
-                    <td>Commentaire / Tutoriel</td>
-                    <td><?= $data->tutoriel->title; ?><br>
-                        <i>Commentaire: <?= $data->id; ?></i>
-                    </td>
-                    <td>Ce commentaire n'est pas poster</td>
-                    <td class="text-right">
-                        <a href=""><i class="la la-eye"></i> </a>
-                    </td>
-                </tr>
-            <?php endif; ?>
-        <?php endforeach; ?>
-        <?php
-        $content = ob_get_clean();
-
-        return $content;
+        return $this->sendResponse($arrs, "Contenue");
     }
 }
